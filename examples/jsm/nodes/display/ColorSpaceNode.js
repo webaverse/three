16165909bc5e18ee562da355afae43861779092f
Avaer Kazmer <a@modules.io>
2022-08-06 03:46:55 -0400

    Remove sRGBToLinear

--- examples/jsm/nodes/display/ColorSpaceNode.js
+++ examples/jsm/nodes/display/ColorSpaceNode.js
@@ -9,14 +9,6 @@ export const LinearToLinear = new ShaderNode( ( inputs ) => {
 
 } );
 
-export const sRGBToLinear = new ShaderNode( ( inputs ) => {
-
-	const { value } = inputs;
-
-	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
-
-} );
-
 export const LinearTosRGB = new ShaderNode( ( inputs ) => {
 
 	const { value } = inputs;
@@ -35,8 +27,7 @@ export const LinearTosRGB = new ShaderNode( ( inputs ) => {
 
 const EncodingLib = {
 	LinearToLinear,
-	LinearTosRGB,
-	sRGBToLinear,
+	LinearTosRGB
 };
 
 class ColorSpaceNode extends TempNode {
--- src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/encodings_pars_fragment.glsl.js
@@ -1,9 +1,5 @@
 export default /* glsl */`
 
-vec4 sRGBToLinear( in vec4 value ) {
-	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
-}
-
 vec4 LinearToLinear( in vec4 value ) {
 	return value;
 }
