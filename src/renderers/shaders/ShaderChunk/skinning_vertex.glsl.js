4d460b3a64bf7fb27ebd49111cd1fd9deb2e5707
Jack Z <jDevlin685@outlook.com>
2022-08-11 19:11:37 +0800

    Updated shader for bone chain accuracy fix

--- src/renderers/shaders/ShaderChunk/skinning_vertex.glsl.js
+++ src/renderers/shaders/ShaderChunk/skinning_vertex.glsl.js
@@ -1,7 +1,7 @@
 export default /* glsl */`
 #ifdef USE_SKINNING
 
-	vec4 skinVertex = bindMatrix * vec4( transformed, 1.0 );
+	vec4 skinVertex = vec4( transformed, 1.0 );
 
 	vec4 skinned = vec4( 0.0 );
 	skinned += boneMatX * skinVertex * skinWeight.x;
@@ -9,7 +9,7 @@ export default /* glsl */`
 	skinned += boneMatZ * skinVertex * skinWeight.z;
 	skinned += boneMatW * skinVertex * skinWeight.w;
 
-	transformed = ( bindMatrixInverse * skinned ).xyz;
+	transformed = skinned.xyz;
 
 #endif
 `;
